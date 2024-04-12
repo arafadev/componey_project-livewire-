411a63e280edca145efb8fbd8aa8ef183ead6492
Arafa Dev <arafa.dev@gmail.com>
2024-04-12 06:10:53 +0200

    front theme integration

--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,29 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
+
+
+Route::prefix('/')->name('front.')->group(function () {
+    Route::view('', 'front.index')->name('index');
+    Route::view('about', 'front.about')->name('about');
+    Route::view('contact', 'front.contact')->name('contact');
+    Route::view('projects', 'front.projects')->name('projects');
+    Route::view('services', 'front.services')->name('services');
+    Route::view('team', 'front.team')->name('team');
+    Route::view('testimonial', 'front.testimonial')->name('testimonial');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
